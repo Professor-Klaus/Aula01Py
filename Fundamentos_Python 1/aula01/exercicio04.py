247abece76c2e89f9bd3b49276900cc7458fa03a
Professor-Klaus <gregory.kspereira@senacsp.edu.br>
2025-07-03 19:16:36 -0300

    Add files via upload

---
+++ Fundamentos_Python 1/aula01/exercicio04.py
@@ -0,0 +1,5 @@
+print (' Programa de soma')
+numero01 = int(input('Digite o primeiro numero: '))
+numero02 = int(input('Digite o segundo numero: '))
+resultadoSoma = numero01+numero02
+print('O resultado da soma é:',resultadoSoma)
